c3cfd669c37fdf2ed8fa7abedab3f8a69e822515
vsion <vsionzhao@trussan.com>
2019-11-21 18:16:16 +0800

    添加移动文件命令

--- watch/execute.js
+++ watch/execute.js
@@ -3,7 +3,12 @@ const utils  = require('../utils');
 async function execute(event, path) {
   const buildDir = resolve(`${config.WATCH_PATH}`);
   const rmFile = resolve(`${config.WATCH_PATH}/${config.BUILD_PATH_NAME}`);
-  const commons = [`rm -rf ${rmFile}`, `cd ${buildDir} && cnpm install`, `cd ${buildDir} && npm run build`];
+  const commons = [
+    `rm -rf ${rmFile}`,
+    `cd ${buildDir} && npm install`,
+    `cd ${buildDir} && npm run build`,
+    `cp -R ${rmFile}/* /home/web`,
+  ];
 
   // 不要用有回调的方法去调用await修饰的方法
   for (const common of commons){
